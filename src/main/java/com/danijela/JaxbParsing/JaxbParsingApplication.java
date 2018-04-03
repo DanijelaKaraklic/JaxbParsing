f6fc5caaaa9912bea5dc41dc0674506269a372de
Danijela <danijela.popovic987@gmail.com>
2018-04-03 13:05:29 +0200

    Added jaxb plugin

---
+++ src/main/java/com/danijela/JaxbParsing/JaxbParsingApplication.java
@@ -0,0 +1,12 @@
+package com.danijela.JaxbParsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxbParsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxbParsingApplication.class, args);
+	}
+}
